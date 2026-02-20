4a78604ad5369213e3bc7853083a4e1b081212e0
Naz T <tastanovan08@gmail.com>
2026-02-21 04:35:34 +0500

    second task

--- 2 lab work/Lab 2.py
+++ 2 lab work/Lab 2.py
@@ -38,3 +38,63 @@ result.write("Самый активный покупатель:" + max_user + "\
 result.write("Средний чек:" + str(average) + "\n")
 
 result.close()
+
+
+#2
+import csv
+file = open("employees.csv", "r")
+reader = csv.reader(file)
+next(reader)
+total_salary = 0
+count = 0
+departs = {}
+names = []
+max_zhumyshy = None
+max_salary = None
+for row in reader:
+    zhumyshy = row[0]
+    salary = int(row[2])
+    department = row[1]
+    names.append({
+        "name": zhumyshy,
+        "department": department,
+        "salary": salary
+    })
+    total_salary += salary
+    count += 1
+    if department not in departs:
+        departs[department] = []
+        departs[department].append(salary)
+    if max_salary is None or salary > max_salary:
+        max_salary = salary
+        max_zhumyshy = zhumyshy
+average = total_salary / len(names)
+print("Средняя зарплата: ", average)
+print(departs)
+departs_avg = {}
+for dep in departs:
+    dep_total = 0
+    dep_count = 0
+    for sal in departs[dep]:
+        dep_total += sal
+        dep_count += 1
+    departs_avg[dep] = dep_total / dep_count
+    print(f"{dep}: {departs_avg[dep]}")
+
+best_department = None
+best_avg = None
+
+for dep in departs_avg:
+    if best_avg is None or departs_avg[dep] > best_avg:
+        best_avg = departs_avg[dep]
+        best_department = dep
+
+print("Отдел с самой высокой средней зарплатой:", best_department)
+
+high_salary = []
+
+for emp in names:
+    if emp["salary"] > average:
+        high_salary.append(emp)
+        print(emp["name"], emp["salary"])
+print(max_zhumyshy)
